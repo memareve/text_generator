ac67a3ea125ddcc42903ff786d20fbef2cb0c6cf
memareve <yqqN87cjbeER>
2021-03-31 18:21:48 +0700

    beta

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import random
 
 
 def main():
-    name_file = input()
-    number_of_sent = int(input())
+    name_file = input('Имя файла: ')
+    number_of_sent = int(input('Количество генерируемых предложений: '))
     return name_file, number_of_sent
 
 
@@ -18,7 +18,9 @@ def dictionary(name):
     with open(name, 'r', encoding="utf8") as f_in:
         a = []
         for i in f_in:
-            i = i.replace('\n','').replace('!','').replace('?','').replace('.','').split()
+            i = i.replace('\n', '').replace('!', '').replace('?', '').replace('.', '').replace('(','').replace(')','')\
+                .replace('<','').replace('>','').replace('\'','').replace('"','').replace('«','').replace('»','')\
+                .replace('-','').replace('—','').replace(';','').lower().split()
             a += i
     d = {}
     for i in range(len(a)-1):
@@ -27,28 +29,46 @@ def dictionary(name):
             d.setdefault(a[i], []).append(i+1)
         else:
             d.setdefault(a[i], []).append(i+1)
-    print(a)
-    print(d)
+    d.setdefault(a[-1], []).append(0)
     return a, d
 
-def first(array):
-    f = []
-    for i in array:
-        if i[0].isupper() == True:
-            f.append(i)
-    first_word = random.choice(f)
-    return first_word
 
-def generation(array, dicti, fiirst):
+def generation(array, dicti, n_sent):
     sent = []
     text = []
-    sent.append(fiirst)
-    sent.append(random.choice(dicti))
-    print(sent)
+    cap = 1
+    for i in range(int(n_sent)):
+        word = random.choice(array)
+        word = array.index(word)
+        if cap == 1:
+            sent.append(array[word].capitalize())
+        else:
+            sent.append(array[word])
+        n_words = random.randint(6,22)
+        for j in range(n_words):
+            list = dicti[str(array[int(word)])]
+            word = array[random.choice(list)]
+            sent.append(word)
+            word = array.index(word)
+        if sent[-1][-1] not in ',:;-':
+            sent[-1] += random.choice(['.','!','?'])
+            cap = 1
+        else:
+            cap = 0
+        text += sent
+        sent = []
+    lenn = 0
+    for i in text:
+        if lenn + len(i) > 96:
+            print('')
+            print(i, end=' ')
+            lenn = len(i) + 1
+            continue
+        print(i, end=' ')
+        lenn += len(i) + 1
 
 
 fil, numb = main()
 ar, di = dictionary(fil)
-ffirst = first(ar)
-generation(ar,di,ffirst)
+generation(ar, di, numb)
 
