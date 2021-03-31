315d8b5be593979beb55c403d0f11de464242c10
memareve <yqqN87cjbeER>
2021-03-31 12:43:33 +0700

    start

--- main.py
+++ main.py
@@ -4,3 +4,51 @@
 # Evdischenko M. (%)
 
 
+import random
+
+
+
+def main():
+    name_file = input()
+    number_of_sent = int(input())
+    return name_file, number_of_sent
+
+
+def dictionary(name):
+    with open(name, 'r', encoding="utf8") as f_in:
+        a = []
+        for i in f_in:
+            i = i.replace('\n','').replace('!','').replace('?','').replace('.','').split()
+            a += i
+    d = {}
+    for i in range(len(a)-1):
+        if a[i] not in d:
+            d[a[i]] = []
+            d.setdefault(a[i], []).append(i+1)
+        else:
+            d.setdefault(a[i], []).append(i+1)
+    print(a)
+    print(d)
+    return a, d
+
+def first(array):
+    f = []
+    for i in array:
+        if i[0].isupper() == True:
+            f.append(i)
+    first_word = random.choice(f)
+    return first_word
+
+def generation(array, dicti, fiirst):
+    sent = []
+    text = []
+    sent.append(fiirst)
+    sent.append(random.choice(dicti))
+    print(sent)
+
+
+fil, numb = main()
+ar, di = dictionary(fil)
+ffirst = first(ar)
+generation(ar,di,ffirst)
+
